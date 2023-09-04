5590bf9db5c5446ea6bfe51188f4e26ecf16e715
r1chwo0n <woonzy09@gmail.com>
2023-09-04 19:44:26 +0700

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,248 @@
+"use client";
+import { useState } from "react";
+
+export default function RegisFormPage() {
+  const [fname, setFname] = useState("");
+  const [fnameError, setFnameError] = useState(false);
+  const [lname, setLname] = useState("");
+  const [lnameError, setLnameError] = useState(false);
+  const [plan, setPlan] = useState(""); //ยังไม่เลือกอะไรให้เป็น string ว่างเปล่า
+  const [planError, setPlanError] = useState(false);
+  const [gender, setGender] = useState(null);
+  const [genderError, setGenderError] = useState(false);
+  const [buyBottle, setBuyBottle] = useState(false);
+  const [buyShoes, setBuyShoes] = useState(false);
+  const [buyCap, setBuyCap] = useState(false);
+  const [isUserAgree, setAgree] = useState(false);
+
+  const inputFnameOnChange = (event) => {
+    setFnameError(false);
+    setFname(event.target.value);
+  };
+
+  const inputLnameOnChange = (event) => {
+    setLnameError(false);
+    setLname(event.target.value);
+  };
+
+  const selectPlanOnChange = (event) => {
+    //รันเมื่อ select เปลี่ยนค่า
+    setPlanError(false);
+    setPlan(event.target.value);
+  };
+
+  const radioGenderMaleOnChange = () => {
+    //ถูกclickป่าว ไม่่ได้เอาไปใช้อะไรต่อ
+    setGenderError(false);
+    setGender("male");
+  };
+
+  const radioGenderFemaleOnChange = () => {
+    setGenderError(false);
+    setGender("female");
+  };
+
+  const cbAgreeOnChange = (event) => {
+    //check box
+    setAgree(event.target.checked); //ติ้กถูกส่งtrue
+  };
+
+  const cbBuyBottleOnChange = (event) => {
+    setBuyBottle(event.target.checked);
+  };
+
+  const cbBuyShoesOnChange = (event) => {
+    setBuyShoes(event.target.checked);
+  };
+
+  const cbBuyCapOnChange = (event) => {
+    setBuyCap(event.target.checked);
+  };
+
+  function computeTotalPayment() {
+    let total = 0;
+    if (plan === "funrun") total += 500;
+    if (plan === "mini") total += 800;
+    if (plan === "half") total += 1200;
+    if (plan === "full") total += 1500;
+    if (buyBottle) total += 200;
+    if (buyShoes) total += 600;
+    if (buyCap) total += 400;
+    if (buyBottle && buyCap && buyShoes) {
+      total *= 0.8;
+    }
+    return total;
+  }
+
+  const registerBtnOnClick = () => {
+    let fnameOk = true;
+    if (fname === "") {
+      fnameOk = false;
+      setFnameError(true);
+    }
+
+    let lnameOk = true;
+    if (lname === "") {
+      lnameOk = false;
+      setLnameError(true);
+    }
+
+    let planOk = true;
+    if (plan === "") {
+      planOk = false;
+      setPlanError(true);
+    }
+
+    let genderOk = true;
+    if (gender !== "male" && gender !== "female") {
+      genderOk == false;
+      setGenderError(true);
+    }
+
+    if (fnameOk && lnameOk && planOk && genderOk) {
+      alert(
+        `Registration complete. Please pay money for ${computeTotalPayment().toLocaleString()} THB.`
+      );
+    }
+  };
+
+  return (
+    <div className="mx-auto vstack gap-3" style={{ width: "400px" }}>
+      <h3 className="text-center fst-italic">Register CMU Marathon 🏃‍♂️</h3>
+      {/* First name & Last name */}
+      <div className="d-flex gap-2">
+        <div>
+          <label className="form-label">First name</label>
+          <input
+            className={"form-control" + (fnameError ? " is-invalid" : "")}
+            onChange={inputFnameOnChange}
+            value={fname}
+          />
+          <div className="invalid-feedback">Invalid first name</div>
+        </div>
+        <div>
+          <label className="form-label">Last name</label>
+          <input
+            className={"form-control" + (lnameError ? " is-invalid" : "")}
+            onChange={inputLnameOnChange}
+            value={lname}
+          />
+          <div className="invalid-feedback">Invalid last name</div>
+        </div>
+      </div>
+
+      {/* Running Plan */}
+      <div>
+        <label className="form-label">Plan</label>
+        <select
+          className={"form-control" + (planError ? " is-invalid" : "")}
+          onChange={selectPlanOnChange}
+          value={plan}
+        >
+          {/* แสดงoptionให้เลือก */}
+          <option value="">Please select..</option>
+          <option value="funrun">Fun run 5.5 Km (500 THB)</option>
+          <option value="mini">Mini Marathon 10 Km (800 THB)</option>
+          <option value="half">Half Marathon 21 Km (1,200 THB)</option>
+          <option value="full">Full Marathon 42.195 Km (1,500 THB)</option>
+        </select>
+        <div className="invalid-feedback">Please select a Plan</div>
+      </div>
+
+      {/* Gender */}
+      <div>
+        <label className="form-label">Gender</label>
+        <div>
+          <input
+            className="me-2 form-check-input"
+            type="radio"
+            onChange={radioGenderMaleOnChange}
+            checked={gender === "male"} //ถูกติ้กเมื่อ gender = male
+          />
+          Male 👨
+          <input
+            className="mx-2 form-check-input"
+            type="radio"
+            onChange={radioGenderFemaleOnChange}
+            checked={gender === "female"} //ถูกติ้กเมื่อ gender = female
+          />
+          Female 👩
+          {/* To show error when user did not select gender, */}
+          {/* We just have to render the div below (Not using is-invalid bootstrap class) */}
+          {/* <div className="text-danger">Please select gender</div> */}
+          {genderError && (
+            <div className="text-danger">Please select gender</div>
+          )}
+        </div>
+      </div>
+
+      {/* Extra Items */}
+      <div>
+        <label className="form-label">Extra Item(s)</label>
+        <div>
+          <input
+            className="form-check-input"
+            type="checkbox"
+            onChange={cbBuyBottleOnChange}
+            checked={buyBottle}
+          />{" "}
+          <label className="form-check-label">Bottle 🍼 (200 THB)</label>
+        </div>
+        <div>
+          <input
+            className="form-check-input"
+            type="checkbox"
+            onChange={cbBuyShoesOnChange}
+            checked={buyShoes}
+          />{" "}
+          <label className="form-check-label">Shoes 👟 (600 THB)</label>
+        </div>
+        <div>
+          <input
+            className="form-check-input"
+            type="checkbox"
+            onChange={cbBuyCapOnChange}
+            checked={buyCap}
+          />{" "}
+          <label className="form-check-label">Cap 🧢 (400 THB)</label>
+        </div>
+      </div>
+
+      <div className="alert alert-primary" role="alert">
+        Promotion📢 Buy all items to get 20% Discount
+      </div>
+
+      {/* Total Payment */}
+      <div>
+        Total Payment : {computeTotalPayment().toLocaleString()} THB
+        {/* Render below element conditionally when user get 20% discount */}
+        {/* <span className="text-success d-block">(20% Discounted)</span> */}
+        {buyBottle && buyCap && buyShoes && (
+          <span className="text-success d-block">(20% discounted)</span>
+        )}
+      </div>
+
+      {/* Terms and conditions */}
+      <div>
+        <input
+          className="me-2"
+          type="checkbox"
+          onChange={cbAgreeOnChange}
+          checked={isUserAgree}
+        />
+        I agree to the terms and conditions
+      </div>
+
+      {/* Register Button */}
+      <button
+        className="btn btn-success my-2"
+        onClick={registerBtnOnClick}
+        disabled={!isUserAgree}
+        //You can embbed a state like below to disabled the button
+        //disabled={isUserAgreed}
+      >
+        Register
+      </button>
+    </div>
+  );
+}
